952cbf39b2ef6bdef33e1053ff60839a9c6cfc20
안예진 <ahw24033@naver.com>
2022-02-23 19:09:24 +0900

    ActivateForm added

--- src/components/activate/ActivateForm.jsx
+++ src/components/activate/ActivateForm.jsx
@@ -56,36 +56,12 @@ function ActivateForm({ activateId, handleDidSave }) {
   );
 
   useEffect(() => {
-    // 서버로 photo=null이 전달이 되면, 아래 오류가 발생
-    //   - The submitted data was not a file. Check the encoding type on the form.
-    //   - 대응 : fieldValues에서 photo만 제거해주거나, photo=null이라면 빈 문자열로 변경
-    // setFieldValues((prevFieldValues) => ({
-    //   ...prevFieldValues,
-    //   photo: '',
-    // }));
-
-    // 인자 1개를 받는 함수를 리턴 : 원본
-    // 함수(원본) => 변경된 사본을 리턴;
     setFieldValues(
       produce((draft) => {
         draft.photo = '';
       }),
     );
 
-    // immer 2단계
-    // setFieldValues((prevFieldValues) => {
-    //   return produce(prevFieldValues, (draft) => {
-    //     draft.photo = '';
-    //   });
-
-    // immer 3단계
-    // setFieldValues((prevFieldValues) =>
-    //   produce(prevFieldValues, (draft) => {
-    //     draft.photo = '';
-    //   }),
-    // );
-
-    // immer 4단계
     setFieldValues(
       produce((draft) => {
         draft.photo = '';
